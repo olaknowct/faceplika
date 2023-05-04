7d54efa5332e80116a99b990bf092e0c22599f1c
Christopher Olano <olanochristopher@gmail.com>
2023-05-04 16:25:23 +0800

    server : implement register user using multer and bcrypt

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import multer from 'multer';
+import { register } from './controllers/auth.js';
 
 // use express framewrok
 const app = express();
@@ -14,6 +16,20 @@ const PORT = process.env.PORT || 6001;
 // Replacing password
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+// File Storage
+const storage = multer.diskStorage({
+  destination: function (erq, file, cb) {
+    cb(null, 'public/assets');
+  },
+
+  filename: function (erq, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/auth/register', upload.single('picture'), register);
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
